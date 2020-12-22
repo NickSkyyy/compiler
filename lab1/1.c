89bbc329205aad77474ad8baf8340cbf014ef951
NickSkyyy <1755987589@qq.com>
2020-12-22 23:59:40 +0800

    fix some ignore

--- lab1/1.c
+++ lab1/1.c
@@ -1,15 +1,9 @@
 #include <stdio.h>
+int noval1, noval2;
+int ival = 412;
+char a = 'a';
+const int maxn = 10000;
 int main()
 {
-    int n, i, f;
-    scanf("%d", &n);
-    i = 2;
-    f = 1;
-    while (i <= n)
-    {
-        f = f * i;
-        i = i + 1;
-    }
-    printf("%d\n", f);
     return 0;
 }
--- lab5/tests/idt.c
+++
@@ -1,6 +0,0 @@
-const int MAXN = 100;
-int main()
-{
-    putint(100);
-    putch('\n');
-}
